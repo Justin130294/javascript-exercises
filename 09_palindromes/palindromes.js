15078f80fe4e741edfec99b71616c6f2a3456043
Justin <justinngthianhuat@hotmail.com>
2022-02-19 23:47:01 +0800

    completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // remove all punctuation
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    let rawLetters = string.replace(regex, '');
+    // remove all spaces
+    transformedString = rawLetters.replace(/\s+/g, '');
 
+    // Change all to lower case
+    transformedString = transformedString.toLowerCase();
+
+    // Create reverse string
+    transformedStringArray = transformedString.split('');
+    reverseStringArray = transformedStringArray.reverse();
+    reverseString = reverseStringArray.join('');
+
+    return transformedString === reverseString;
 };
 
 // Do not edit below this line
